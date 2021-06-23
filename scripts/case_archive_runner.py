a1cdd3d98885b4cb2e3128301cc2be374a5b9303
Nick Anthony <nanthony007@gmail.com>
2021-06-23 14:20:23 -0400

    Add case archive script runner.

---
+++ scripts/case_archive_runner.py
@@ -0,0 +1,11 @@
+from geocoding import case_archive_utils as cau
+
+if __name__ == '__main__':
+    print('Loading data...')
+    df = cau.load_case_archive_data()
+    print('Geocoding case archive data...')
+    geocoded_df = cau.geocode_case_archive(df)
+    print('Calculating distances...')
+    distance_df = cau.calculate_distance(geocoded_df)
+    print('Dumping data...')
+    cau.dump_case_archive_data(distance_df)
