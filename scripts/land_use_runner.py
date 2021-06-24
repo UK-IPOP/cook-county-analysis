bd0c7ec584243e670e9078e6683d088ec8248eee
Nick <nanthony007@gmail.com>
2021-06-24 13:05:39 -0400

    Fix filepath.

--- scripts/land_use_runner.py
+++ scripts/land_use_runner.py
@@ -1,12 +1,14 @@
 import geopandas as gpd
 import pandas as pd
 
-if __name__ == '__main__':
-    print('Reading datasets...')
-    land_use_shapes = gpd.read_file('./resources/Chicago_LandUse_Shapes/Landuse2015_CMAP_v1.shp')
-    data_dict = pd.read_csv('./data/land_use_data_dictionary.csv')
-    print('Merging datasets...')
-    data_dict['LANDUSE'] = data_dict.landuse_id.apply(lambda x: str(x))
-    geo_df = land_use_shapes.merge(data_dict, on='LANDUSE')
-    print('Writing new shapefile...')
-    geo_df.to_file('./data/LANDUSE_SHAPES/land_use.shp')
+if __name__ == "__main__":
+    print("Reading datasets...")
+    land_use_shapes = gpd.read_file(
+        "./resources/Chicago_LandUse_Shapes/Landuse2015_CMAP_v1.shp"
+    )
+    data_dict = pd.read_csv("./data/landuse_data_dictionary.csv")
+    print("Merging datasets...")
+    data_dict["LANDUSE"] = data_dict.landuse_id.apply(lambda x: str(x))
+    geo_df = land_use_shapes.merge(data_dict, on="LANDUSE")
+    print("Writing new shapefile...")
+    geo_df.to_file("./data/LANDUSE_SHAPES/land_use.shp")
