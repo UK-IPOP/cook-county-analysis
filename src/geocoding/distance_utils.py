baf0c7b816e54cffa1e725ec9442878211ea8bad
Nick <nanthony007@gmail.com>
2021-06-25 11:46:59 -0400

    Improve performance of distance calculation.

--- src/geocoding/distance_utils.py
+++ src/geocoding/distance_utils.py
@@ -1,16 +1,32 @@
 from operator import index
+from typing import Union
 from geopy import distance
 import pandas as pd
+from tqdm import tqdm
 
-from . import case_archive_utils as cau, pharmacy_utils as pu
+tqdm.pandas()
 
 
-def calculate_distance(row: pd.Series, pharm_df: pd.DataFrame) -> float:
+def get_pharmacy_points() -> list[tuple[float, float]]:
+    points = []
+    pharmacies = pd.read_csv("./data/geocoded_pharmacies.csv")
+    for _, pharm in pharmacies.iterrows():
+        if pd.notna(pharm.coded_lat) and pd.notna(pharm.coded_long):
+            point = (pharm.coded_lat, pharm.coded_long)
+            points.append(point)
+    return points
+
+
+def calculate_distance(
+    row: pd.Series, pharm_points: list[tuple[float, float]]
+) -> Union[float, None]:
     distances = []
-    for _, pharm in pharm_df.iterrows():
+    if pd.isna(row.coded_lat) or pd.isna(row.coded_long):
+        return None
+    for p in pharm_points:
         d = distance.distance(
             (row.coded_lat, row.coded_long),  # case archive (point1)
-            (pharm.coded_lat, pharm.coded_long),  # pharmacy (point2)
+            (p[0], p[1]),  # pharmacy (point2)
         )
         # miles
         distances.append(d.mi)
@@ -18,9 +34,9 @@ def calculate_distance(row: pd.Series, pharm_df: pd.DataFrame) -> float:
 
 
 def apply_distance():
-    case_archives = pd.read_csv("./data/geocoded_case_archives.csv")
-    pharmacies = pd.read_csv("./data/geocoded_pharmacies.csv")
-    case_archives["distance"] = case_archives.apply(
-        lambda row: calculate_distance(row, pharm_df=pharmacies), axis=1
+    case_archives = pd.read_csv("./data/geocoded_case_archives.csv", low_memory=False)
+    pharm_point_list = get_pharmacy_points()
+    case_archives["nearest_pharmacy"] = case_archives.progress_apply(
+        lambda row: calculate_distance(row, pharm_point_list), axis=1
     )
     case_archives.to_csv("./data/case_archives_distances.csv", index=False)
