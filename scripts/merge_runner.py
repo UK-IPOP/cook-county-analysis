a72c6305e6220a1f13d571f2b6e2716381451e4c
Nick <nanthony007@gmail.com>
2021-07-09 15:03:57 -0400

    Add file to merge drug and spatial-join datasets.

---
+++ scripts/merge_runner.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+if __name__ == "__main__":
+    print("Loading files...")
+    df1 = pd.read_csv("./data/joined_records.csv", low_memory=False)
+    df2 = pd.read_csv("./data/drug_classifications.csv", low_memory=False)
+    print("Merging datasets...")
+    df3 = pd.merge(df1, df2, left_on="Case Number", right_on="primary_id")
+    print('Dumping datafiles...')
+    df3.to_csv('./data/output.csv', index=False)
