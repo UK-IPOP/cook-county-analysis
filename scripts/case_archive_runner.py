1256806c574c8b1305b0ed6c23629173a1da25de
Nick <nanthony007@gmail.com>
2021-07-09 17:42:05 -0400

    Remove live/not-live feature.

--- scripts/case_archive_runner.py
+++ scripts/case_archive_runner.py
@@ -3,11 +3,11 @@ from geocoding import case_archive_utils as cau
 # set this to `False` this if you have a specific file
 # the file must be `Medical_Examiner_Case_Archive.csv`
 # this may also cause some column naming conventions to break
-LIVE = True
+# LIVE = True
 
 if __name__ == "__main__":
     print("Loading data...")
-    df = cau.get_live_case_archive_data() if LIVE else cau.load_case_archive_data()
+    df = cau.get_live_case_archive_data()
     print("Geocoding case archive data...")
     geocoded_df = cau.geocode_case_archive(df)
     print("Calculating distances...")
