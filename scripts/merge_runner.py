43ad1c9ecd0542ce1bf09847d1755298d48c64fa
Nick <nanthony007@gmail.com>
2021-07-09 15:24:20 -0400

    Add done statement.

--- scripts/merge_runner.py
+++ scripts/merge_runner.py
@@ -6,5 +6,6 @@ if __name__ == "__main__":
     df2 = pd.read_csv("./data/drug_classifications.csv", low_memory=False)
     print("Merging datasets...")
     df3 = pd.merge(df1, df2, left_on="Case Number", right_on="primary_id")
-    print('Dumping datafiles...')
-    df3.to_csv('./data/output.csv', index=False)
+    print("Dumping datafiles...")
+    df3.to_csv("./data/output.csv", index=False)
+    print("Done.")
