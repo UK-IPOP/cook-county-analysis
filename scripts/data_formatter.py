5b7682911b05dde5db1d877b602e468dcef4cb82
Nick Anthony <nanthony007@gmail.com>
2021-08-19 10:56:53 -0400

    Add support for datetime columns.

--- scripts/data_formatter.py
+++ scripts/data_formatter.py
@@ -1,5 +1,15 @@
 import pandas as pd
 
+
+def extract_date_data(df: pd.DataFrame):
+    df["death_date"] = df.death_date.apply(lambda x: pd.to_datetime(x))
+    df["death_time"] = df.death_date.apply(lambda x: x.time() if pd.notna(x) else pd.NA)
+    df["death_date"] = df.death_date.apply(lambda x: x.date())
+    df["death_year"] = df.death_date.apply(lambda x: x.year)
+    df["death_month"] = df.death_date.apply(lambda x: x.month)
+    df["death_day"] = df.death_date.apply(lambda x: x.day)
+
+
 if __name__ == "__main__":
     print("Starting final formatting.")
     df = pd.read_csv("./data/extracted_drugs.csv", low_memory=False)
@@ -21,6 +31,7 @@ if __name__ == "__main__":
         "index_right",
     ]
     df.drop(not_needed_cols, axis=1, inplace=True)
+    extract_date_data(df)
     print("Data formatted, writing to csv")
     df.to_csv("./data/output.csv", index=False)
     print("Done.")
