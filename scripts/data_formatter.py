7f1ed31689a1043a73a199bf5ce47a3d8d332936
Nick Anthony <nanthony007@gmail.com>
2021-08-25 10:15:48 -0400

    Adjust notneeded cols.

--- scripts/data_formatter.py
+++ scripts/data_formatter.py
@@ -12,7 +12,7 @@ def fill_nulls(df: pd.DataFrame):
 
 
 def extract_date_data(df: pd.DataFrame):
-    df["death_date"] = df.death_date.apply(lambda x: pd.to_datetime(x))
+    df["death_date"] = df.date_of_death.apply(lambda x: pd.to_datetime(x))
     df["death_time"] = df.death_date.apply(lambda x: x.time() if pd.notna(x) else pd.NA)
     df["death_date"] = df.death_date.apply(lambda x: x.date())
     df["death_year"] = df.death_date.apply(lambda x: x.year)
@@ -24,21 +24,20 @@ if __name__ == "__main__":
     print("Starting final formatting.")
     df = pd.read_csv("./data/extracted_drugs.csv", low_memory=False)
     not_needed_cols = [
-        "incident_street",
+        "incident_address",
         "incident_city",
-        "incident_zip",
+        "incident_zip_code",
         "longitude",
         "latitude",
         "location",
         "residence_city",
         "residence_zip",
         "objectid",
-        ":@computed_region_tu5p_2ban",
-        ":@computed_region_nqe2_pztc",
-        ":@computed_region_h3ai_7k6i",
+        # ":@computed_region_tu5p_2ban",
+        # ":@computed_region_nqe2_pztc",
+        # ":@computed_region_h3ai_7k6i",
         "clean_address",
         "geometry",
-        "index_right",
     ]
     df.drop(not_needed_cols, axis=1, inplace=True)
     extract_date_data(df)
