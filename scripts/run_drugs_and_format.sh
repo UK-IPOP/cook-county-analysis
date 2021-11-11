8497445f7a73b9428717171e2e888ca7afa0d62d
Nick Anthony <nanthony007@gmail.com>
2021-11-11 14:15:46 -0500

    Move to separate files for before/after spatial join

--- scripts/run_drugs_and_format.sh
+++ scripts/run_drugs_and_format.sh
@@ -1,18 +1,5 @@
 #! /usr/bin/env bash
 
-# python to load, preprocess, and geocode
-
-echo "Running data processing"
-poetry run python ./scripts/data_runner.py
-
-# golang to calculate distance
-
-echo "Bulding Go program"
-./scripts/build.sh
-
-echo "Running distance calculation"
-./scripts/calculate-distance
-
 echo "Extracting drugs"
 # can run on clean cases because does not need geo-spatial information
 ./scripts/drug-extraction pipeline "./data/processed/clean_cases.csv" --id-col "casenumber" --target-col "combined_primary" --clean --format --format-type "csv"
