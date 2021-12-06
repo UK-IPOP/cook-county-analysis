fa1f84903daa4d1df599b390d21cc8a5982d46f7
Nick Anthony <nanthony007@gmail.com>
2021-12-06 13:08:25 -0500

    Fix column typo.

--- scripts/run_drugs_and_format.sh
+++ scripts/run_drugs_and_format.sh
@@ -1,15 +1,16 @@
 #! /usr/bin/env bash
 
 echo "Extracting drugs"
-# can run on clean cases because does not need geo-spatial information
-./scripts/drug-extraction pipeline "./data/processed/clean_cases.csv" --id-col "casenumber" --target-col "combined_primary" --clean --format --format-type "csv"
-
-cp ./scripts/output.csv ./data/drugs/primary_drugs.csv
 
 # can run on clean cases because does not need geo-spatial information
-./scripts/drug-extraction pipeline "./data/processed/clean_cases.csv" --id-col "casenumber" --target-col "secondarycause" --clean --format --format-type "csv"
+./data/drugs/drug-extraction pipeline "./data/processed/clean_cases.csv" --id-col "casenumber" --target-col "primary_combined" --clean --format --format-type "csv"
 
-cp ./scripts/output.csv ./data/drugs/secondary_drugs.csv
+cp ./data/drugs/output.csv ./data/drugs/primary_drugs.csv
+
+# can run on clean cases because does not need geo-spatial information
+./data/drugs/drug-extraction pipeline "./data/processed/clean_cases.csv" --id-col "casenumber" --target-col "secondarycause" --clean --format --format-type "csv"
+
+cp ./data/drugs/output.csv ./data/drugs/secondary_drugs.csv
 
 echo "Transforming drugs"
 poetry run python ./scripts/transform_drugs.py
