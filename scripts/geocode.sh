11760289c360855dc790e035c53866f82ba9f977
Nick Anthony <nanthony007@gmail.com>
2022-09-14 16:08:47 -0400

    Add geocoder script
    Former-commit-id: 4c58603a7f8087575c308d6f5d8e75488a9c4cfc

---
+++ scripts/geocode.sh
@@ -0,0 +1,15 @@
+
+# this script reads the medical centers file, geocodes them, and writes the results to a new file
+
+while read -r line; do
+    name=$(jq -r '.name' <<< "$line")
+    address=$(jq -r '.address' <<< "$line")
+
+    geocode "$address" \
+        --provider arcgis \
+        --method geocode \
+        --output json \
+        | \
+        jq -c --arg NAME "$name" '{name: $NAME, address, latitude: .lat, longitude: .lng, score}' > data/geocoded_medical_centers.jsonl
+
+done < data/medical_centers.jsonl
