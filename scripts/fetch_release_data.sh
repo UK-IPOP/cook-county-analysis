4a996f6ecfb380c437fd338eba0c5955683782b1
Nick Anthony <nanthony007@gmail.com>
2022-09-14 16:09:06 -0400

    Add release data fetcher script.
    Former-commit-id: a0f283910e8f95e91031235104b618a4c748f09f

--- scripts/fetch_release_data.sh
+++ scripts/fetch_release_data.sh
@@ -2,5 +2,18 @@
 
 # super simple script to fetch the (latest) release data from the github api using `gh` cli
 
-# downloads release assets to assets/ directory
-gh release download latest -R uk-ipop/open-data-pipeline -D assets
+# downloads release assets to downloads dir 
+# AND converts it from csv to jsonlines using `dasel` and `jq`
+
+# requires `gh` cli and `dasel` and `jq` to be installed
+
+# make if not exists
+mkdir -p downloads
+
+echo "Fetching release data from github api..."
+gh release download -R uk-ipop/open-data-pipeline -D downloads --pattern 'cook_county_wide_form.csv' --clobber \
+    && echo "Converting to jsonlines..." \
+    && cat downloads/cook_county_wide_form.csv | dasel -r csv -w json . | jq -c '.' > downloads/wide_records.jsonl \
+    && echo "Removing csv file..." \
+    && rm downloads/cook_county_wide_form.csv \
+    && echo "Done!"
