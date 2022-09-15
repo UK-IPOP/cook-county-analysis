4db465719e5b43d268b6f7f414a8f3378e9c742e
Nick Anthony <nanthony007@gmail.com>
2022-09-15 15:54:45 -0400

    Only need to fetch land use.
    Former-commit-id: f3814453231d7d7baf608fdc4c2e5c4785b5061b

--- scripts/fetch_spatial_data.sh
+++ scripts/fetch_spatial_data.sh
@@ -2,24 +2,16 @@
 
 echo "Starting to download "
 
+# make if not exists
+mkdir -p downloads
+
+
+# ? TODO: Could all of these URLs just be passed into pandas directly? (except landuse which requires unzipping...)
+# * YES
+
 echo "Downloading LandUse Shapefiles..."
 # this is 2015 LandUse
-# source: https://datahub.cmap.illinois.gov/dataset/e2bec582-3a98-499b-97b0-75a039de29cc/resource/60bd80da-c2b7-4243-a30b-775729da8714
-# wget https://datahub.cmap.illinois.gov/dataset/e2bec582-3a98-499b-97b0-75a039de29cc/resource/60bd80da-c2b7-4243-a30b-775729da8714/download/LUI15shapefilev1.zip -P downloads
-
-
-echo "Downloading Census Shapefiles..."
-# these are 2021 census
-# source: https://www.census.gov/geographies/mapping-files/time-series/geo/tiger-line-file.2021.html
-wget https://www2.census.gov/geo/tiger/TIGER2021/TRACT/tl_2021_17_tract.zip -P downloads
-
-
-echo "Downloading Neighborhood Shapefiles..."
-# this uses ODATA! 🙂
-curl https://data.cityofchicago.org/api/odata/v4/y6yq-dbs2 | jq '.value' > downloads/neighborhoods.geojson
-
-
-echo ""
-
+wget https://datahub.cmap.illinois.gov/dataset/e2bec582-3a98-499b-97b0-75a039de29cc/resource/60bd80da-c2b7-4243-a30b-775729da8714/download/LUI15shapefilev1.zip -P downloads
+unzip downloads/LUI15shapefilev1.zip -d downloads/
 
 echo "Done."
